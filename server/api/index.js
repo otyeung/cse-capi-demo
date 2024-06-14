9ffbb338131cca630c06a35981cea0ff6e9692c7
Dennis Yeung <10432376+otyeung@users.noreply.github.com>
2024-06-14 15:07:42 +0800

    relax CORS whitelist to any origin

--- server/api/index.js
+++ server/api/index.js
@@ -23,7 +23,8 @@ const googleFormFields = {
 }
 
 // Define a whitelist of allowed origins for CORS
-const whitelist = ['https://cse-capi-demo.vercel.app', 'http://localhost:3000']
+//const whitelist = ['https://cse-capi-demo.vercel.app', 'http://localhost:3000']
+const whitelist = ['*']
 
 // Configure CORS options
 const corsOptions = {
